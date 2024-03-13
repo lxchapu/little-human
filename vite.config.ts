27bbe8363032aebb5c77d6bf21b5e4369a9d06e9
lxchapu <lxchapu@outlook.com>
2024-03-13 19:42:37 +0800

    fix: only add tracking code in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      entry: 'src/main.tsx',
+    }),
+  ],
 });
